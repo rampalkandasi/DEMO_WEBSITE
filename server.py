fe3b09cc74105e25292008b6a2eda4f6bf4f62f6
rampalkandasi <mailatrampal@yahoo.com>
2020-05-26 22:28:32 +0530

    demo Website

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask , render_template,request,redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return  render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return  render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form ():
+    error = None
+    if request.method == 'POST':
+        data=request.form.to_dict()
+        print(data)
+    return redirect('/thank.html')
+    
